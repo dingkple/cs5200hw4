e3fc4791860bef1b9128f58c47317b95c2194529
dingkple <king.kple@gmail.com>
2015-03-29 17:21:38 -0400

    final version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cs5200hw4
+# 
+# This homework does not contain too much web page designs.
+#
+# For testing the code, please test every page from eclipse.
+#
+# The sql script is also included
--- WebContent/schema.sql
+++ WebContent/schema.sql
@@ -1,5 +1,9 @@
 
+drop database if exists MovieSocialNetwork;
+create database if not exists MovieSocialNetwork;
 
+
+use MovieSocialNetwork;
 
 CREATE TABLE if not exists `user` (
   `username` varchar(45) not null,
@@ -7,47 +11,51 @@ CREATE TABLE if not exists `user` (
   `lastName` varchar(45) DEFAULT NULL,
   `password` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
-  `dateofbirth` timestamp default null,
-  PRIMARY KEY (`id`)
+  `dateofbirth` timestamp default CURRENT_TIMESTAMP,
+  PRIMARY KEY (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-CREATE TABLE `movie` (
+CREATE TABLE if not exists `movie` (
   `id` varchar(45) NOT NULL,
   `title` varchar(256) DEFAULT NULL,
   `posterimage` varchar(256) DEFAULT NULL,
-  `releasedata` varchar(256) DEFAULT NULL,
+  `releasedate` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 
-CREATE TABLE `actor` (
+CREATE TABLE if not exists `actor` (
   `id` varchar(45) NOT NULL,
   `firstName` varchar(45) DEFAULT NULL,
   `lastName` varchar(45) DEFAULT NULL,
-  `dateofbirth` timestamp default null,
+  `dateofbirth` timestamp default CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
-CREATE TABLE `cast` (
+CREATE TABLE if not exists `cast` (
   `castid` varchar(45) NOT NULL,
-  `charactorname` varchar(45) DEFAULT NULL,
+  `charactername` varchar(45) DEFAULT NULL,
   `actorid` varchar(45) DEFAULT NULL,
   `movieid` varchar(45) default null,
   PRIMARY KEY (`castid`),
-  FOREIGN KEY (`actorid`) REFERENCES `actorid` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  FOREIGN KEY (`movieId`) REFERENCES `movie` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  key `castId` (`castid`),
+  constraint `cast2actor`
+  FOREIGN KEY (`actorid`) REFERENCES `actor` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  constraint `cast2movie` FOREIGN KEY (`movieid`) REFERENCES `movie` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
-CREATE TABLE `comment` (
+CREATE TABLE if not exists `comment` (
   `commentid` varchar(45) NOT NULL,
   `comment` varchar(45) DEFAULT NULL,
-  `actorid` varchar(45) DEFAULT NULL,
-  `date` timestamp default null,
+  `date` timestamp default CURRENT_TIMESTAMP,
   `username` varchar(45) default null,
-  `movidId` varchar(45) default null,  
-  PRIMARY KEY (`commentid`)
-  FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  FOREIGN KEY (`movieId`) REFERENCES `movie` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `movieid` varchar(45) default null, 
+  PRIMARY KEY (`commentid`),
+  key `comment2user_idx` (`username`),
+  -- key `comment2movie_idx` (`movieid`),
+  
+  constraint `comment2user` FOREIGN KEY (`username`) REFERENCES `user`(`username`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  constraint `comment2movie` FOREIGN KEY (`movieid`) REFERENCES `movie` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 -- CREATE TABLE `likes` (
@@ -56,9 +64,9 @@ CREATE TABLE `comment` (
 --   KEY `likes2user_idx` (`userId`),
 --   KEY `likes2movie_idx` (`movieId`),
 --   CONSTRAINT `likes2movie` FOREIGN KEY (`movieId`) REFERENCES `movie` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
---   CONSTRAINT `likes2user` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+--   CONSTRAINT `likes2user` FOREIGN KEY (`userId`) REFERENCES `user` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION
 -- ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+-- 
 -- CREATE TABLE `review` (
 --   `userId` int(11) DEFAULT NULL,
 --   `movieId` int(11) DEFAULT NULL,
